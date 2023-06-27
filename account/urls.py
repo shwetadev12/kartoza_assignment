943930e15d7cdb963e3a4e5c1d2e6184cb918f67
shwetadev12 <shwetadevvermaa4@gmail.com>
2023-06-27 22:15:00 +0530

    User update and map view

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserSignupView, UserSigninView, UserSignOutView, UserProfileView
+from .views import UserSignupView, UserSigninView, UserSignOutView, UserProfileView, UserProfileUpdateView, UsersLocationView
 
 
 urlpatterns = [
@@ -7,4 +7,6 @@ urlpatterns = [
     path('signin/', UserSigninView.as_view(), name='signin'),
     path('signout/', UserSignOutView.as_view(), name='signout'),
     path('profile/<int:pk>/', UserProfileView.as_view(), name='profile'),
+    path('update/profile/<int:pk>/', UserProfileUpdateView.as_view(), name='update_profile'),
+    path('map/', UsersLocationView.as_view(), name='map'),
 ]
