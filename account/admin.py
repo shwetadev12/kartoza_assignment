c31da0ef8b1094d92242931c9facaa91861b822e
shwetadev12 <shwetadevvermaa4@gmail.com>
2023-06-27 23:11:41 +0530

    Update user model for admin page login

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 from .models import User
 
 admin.site.register(User)
+
+
+class CustomUserAdmin(DefaultUserAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            qs = qs.filter(pk=request.user.pk)
+        return qs
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- account/models.py
+++ account/models.py
@@ -6,6 +6,7 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=12, blank=True)
     address = models.CharField(max_length=220, blank=True)
     location = models.PointField()
+    is_staff = models.BooleanField(default=True)
 
     groups = models.ManyToManyField(Group, related_name='custom_user_groups')
     user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')
