5777c02a75da070a61cec9057aab6fd7f06a549f
shwetadev12 <shwetadevvermaa4@gmail.com>
2023-06-27 16:52:55 +0530

    User authentication and CI integration

---
+++ account/models.py
@@ -0,0 +1,14 @@
+from django.contrib.gis.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=12, blank=True)
+    address = models.CharField(max_length=220, blank=True)
+    location = models.PointField()
+
+    groups = models.ManyToManyField(Group, related_name='custom_user_groups')
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
